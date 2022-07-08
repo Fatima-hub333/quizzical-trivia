2fbe236771dde443e94e47ef2d551dbbc0353275
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-07-08 16:33:21 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,65 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
+/* eslint-disable react/jsx-no-bind */
+import React, { useState, useEffect } from 'react';
+import Quiz from './components/Quiz';
+import Home from './components/Home';
 import './index.css';
 
 export default function App() {
+  const savedTheme = localStorage.getItem('theme');
 
+  const [isHome, setIsHome] = useState(true);
+  const [theme, setTheme] = useState(savedTheme || 'light');
+
+  // moved formData Up, so both Home.js & Quiz.js can use the state
+  const [formData, setFormData] = useState({
+    category: 'any',
+    difficulty: '',
+    answerType: '',
+    anoumtOfQuestions: '5',
+  });
+
+  function toggleIsHome() {
+    setIsHome((prev) => !prev);
+  }
+
+  const toggleTheme = React.useCallback(() => {
+    setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
+  }, []);
+
+  function handleFormChange(e) {
+    const { name, value } = e.target;
+
+    setFormData((prev) => ({
+      ...prev, [name]: value,
+    }));
+  }
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  return (
+    <div className="app">
+      {
+        isHome
+          ? (
+            <Home
+              toggleIsHome={toggleIsHome}
+              formData={formData}
+              handleFormChange={handleFormChange}
+              theme={theme}
+              toggleTheme={toggleTheme}
+            />
+          )
+          : (
+            <Quiz
+              formData={formData}
+              toggleIsHome={toggleIsHome}
+              toggleTheme={toggleTheme}
+              theme={theme}
+            />
+          )
+      }
+    </div>
+  );
 }
